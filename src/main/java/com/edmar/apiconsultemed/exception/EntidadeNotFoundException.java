6fdf98c37fbbe8cfea7f22216895697c291eff63
edmarsoares <edmar1soares@gmail.com>
2019-09-14 10:34:22 -0300

    atualizando

--- src/main/java/com/edmar/apiconsultemed/exception/EntidadeNotFoundException.java
+++ src/main/java/com/edmar/apiconsultemed/exception/EntidadeNotFoundException.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class EntidadeNotFoundException extends RuntimeException {
 	
 	public EntidadeNotFoundException(String erro) {
-		super(erro);
+		super(erro); 
 	}
 }
