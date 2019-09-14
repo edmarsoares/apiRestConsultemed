45aa4223f07296aefdb03429559d27169cfa7fdb
Ricardo Belo <iesp@ricardobelo.com.br>
2019-09-14 15:32:07 -0300

    Adição resource medico

--- src/main/java/com/edmar/apiconsultemed/medico/resource/MedicoResource.java
+++ src/main/java/com/edmar/apiconsultemed/medico/resource/MedicoResource.java
@@ -20,7 +20,7 @@ import com.edmar.apiconsultemed.medico.Medico;
 import com.edmar.apiconsultemed.medico.service.MedicoService;
 
 @RestController
-@RequestMapping("/api/medicos")
+@RequestMapping("/medicos")
 public class MedicoResource {
 	
 	@Autowired
